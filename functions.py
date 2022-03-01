4b9dfac8a3d1874b9e8b6faf8deba08eed594d00
Andreas Dyve <andreasdyve1@gmail.com>
2022-03-01 18:17:45 +0100

    update

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+'''_______________________Data work functions__________________________'''
+
 # Function to select a dataset from a cell dataframe
 def select_dataset(df, column):
     
@@ -21,4 +23,13 @@ def select_dataset(df, column):
 
     return s
 
+'''___________________________ Model functions__________________________'''
 
+# Function to get errors
+def get_errors(y_train, y_test, y_train_pred, y_test_pred):
+    mse_cycles_train = mean_squared_error(np.power(10, y_train), np.power(10, y_train_pred), squared=False)
+    mse_cycles_test = mean_squared_error(np.power(10, y_test), np.power(10, y_test_pred), squared=False)
+    mape_train = mean_absolute_percentage_error(y_train, y_train_pred)
+    mape_test = mean_absolute_percentage_error(y_test, y_test_pred)
+    
+    return mse_cycles_train, mse_cycles_test, mape_train, mape_test
