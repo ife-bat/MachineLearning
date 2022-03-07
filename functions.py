a8c69c7ac7aacf97aaa63f95c70901362b449a1b
Andreas Dyve <andreasdyve1@gmail.com>
2022-03-07 15:29:27 +0100

    update

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error
+
 
 '''_______________________Data work functions__________________________'''
 
@@ -27,12 +29,17 @@ def select_dataset(df, column):
 
 # Function to get errors
 def get_errors(y_train, y_test, y_train_pred, y_test_pred):
+    mse_train = mean_squared_error(y_train, y_train_pred)
+    mse_test = mean_squared_error(y_test, y_test_pred)
     mse_cycles_train = mean_squared_error(np.power(10, y_train), np.power(10, y_train_pred), squared=False)
     mse_cycles_test = mean_squared_error(np.power(10, y_test), np.power(10, y_test_pred), squared=False)
+    r2_train = r2_score(y_train, y_train_pred)
+    r2_test = r2_score(y_test, y_test_pred)
     mape_train = mean_absolute_percentage_error(y_train, y_train_pred)
     mape_test = mean_absolute_percentage_error(y_test, y_test_pred)
     
-    return mse_cycles_train, mse_cycles_test, mape_train, mape_test
+    return mse_train, mse_test, mse_cycles_train, mse_cycles_test,\
+        r2_train, r2_test, mape_train, mape_test
 
 # Function for scaling?
 
